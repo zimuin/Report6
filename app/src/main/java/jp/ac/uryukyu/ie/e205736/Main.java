02575ce47ef51035ce63b4d1f84806d44cd3cbe8
zimuin <e205736@ie.u-ryukyu.ac.jp>
2021-01-16 20:07:23 +0900

    10th commit

--- app/src/main/java/jp/ac/uryukyu/ie/e205736/Main.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205736/Main.java
@@ -6,7 +6,20 @@ public class Main{
     public static void main(String[] args){
         Dealer dealer = new Dealer();
         dealer.setActions();
-        System.out.println(dealer.getActions());
+        dealer.explainRule();
+        Player player1 = new Player("僕");
+        Rival player2 = new Rival("金持ちおじさん");
+        player1.drawCard();
+        player2.drawCard();
+        System.out.println("相手のカードは"+player2.getPossesionCard().getCardNumber());
+        player2.facialExpression(player1);
+        player1.select(dealer, dealer.getActions());
+        player2.reDrawCard(player1);
+        System.out.println("相手のカードは"+player2.getPossesionCard().getCardNumber());
+        player2.facialExpression(player1);
+        player2.oddsUp(player1,dealer);
+        dealer.judgeButtle(player1, player2);
+
 
         
         
--- app/src/main/java/jp/ac/uryukyu/ie/e205736/Player.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205736/Player.java
@@ -1,5 +1,6 @@
 package jp.ac.uryukyu.ie.e205736;
-
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Player extends Human {
 
@@ -9,8 +10,20 @@ public class Player extends Human {
        super(name);
     }
     
-    public void select(){
-        
+    public void select(Dealer target,ArrayList<String> actions){
+        System.out.println("どうしますか？");
+        int index = 1;
+        for (var action : actions){
+            System.out.println(index+":"+action);
+            index += 1;
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        int actionNumber = scanner.nextInt();
+        if(actionNumber==1){drawCard();}
+        if(actionNumber==2){oddsDoubleUp(target);}
+        if(actionNumber==3){fold(target);}
+        if(actionNumber==4){battle();}
     }
 
     @Override
