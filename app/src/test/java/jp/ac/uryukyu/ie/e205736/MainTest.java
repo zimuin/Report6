a2fdbb8556b4541a73fe17c07342442e9c695c9a
zimuin <e205736@ie.u-ryukyu.ac.jp>
2021-01-18 17:46:35 +0900

    20th commit

--- app/src/test/java/jp/ac/uryukyu/ie/e205736/MainTest.java
+++ app/src/test/java/jp/ac/uryukyu/ie/e205736/MainTest.java
@@ -1,5 +1,21 @@
 package jp.ac.uryukyu.ie.e205736;
 
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MainTest {
-    
+    @Test
+    void startTest() {
+        int defaultChip = 100;
+        int defaultodds = 1;
+        int defaultbetMagnification = 10;
+        Dealer testDealer = new Dealer();
+        Player testPlayer1 = new Player("テスト男１");
+        Rival testPlayer2 = new Rival("テスト用男２");
+        assertEquals(defaultChip,testPlayer1.getPossesionChip());
+        assertEquals(defaultChip,testPlayer2.getPossesionChip());
+        assertEquals(defaultodds,testDealer.getOdds());
+        assertEquals(defaultbetMagnification,testDealer.getBetMagnification());
+    }
 }
